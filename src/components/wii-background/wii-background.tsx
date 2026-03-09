590b10cab3b1e3bfe649e9e468188ceb7a537797
ethankrol <ethanmkrol@gmail.com>
2026-03-09 13:46:01 -0400

    svg is squishin

--- src/components/wii-background/wii-background.tsx
+++ src/components/wii-background/wii-background.tsx
@@ -6,6 +6,12 @@ function WiiBackground() {
             <div className='background'></div>
             <div className = 'bottom-section'>
                 <div className='bottom-bar'></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 25" className="bar-trans" stroke= "#02CDF1" strokeWidth="0.6" preserveAspectRatio="none">
+                        <path d="m 32 25 l -32 0 l 0 -25 q 6 0 15 7 q 9 7 17 7"/>
+                        
+                    </svg>
+                <div className='bottom-tile'></div>
+                <div className='bottom-bar'></div>
             </div>
         </div>
         
